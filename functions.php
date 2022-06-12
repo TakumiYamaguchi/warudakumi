5164bb7e3e1974c5ce83c50e72dff56328d0562e
TakumiYamaguchi <info@ivorytowerlab.jp>
2022-06-12 15:09:30 +0900

    js,css functions.phpから読み込み、WOデフォルトjquery読み込ませない。OK

--- functions.php
+++ functions.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * _s functions and definitions
  *
@@ -7,12 +8,12 @@
  * @package _s
  */
 
-if ( ! defined( '_S_VERSION' ) ) {
+if (!defined('_S_VERSION')) {
 	// Replace the version number of the theme on each release.
-	define( '_S_VERSION', '1.0.0' );
+	define('_S_VERSION', '1.0.0');
 }
 
-if ( ! function_exists( '_s_setup' ) ) :
+if (!function_exists('_s_setup')) :
 	/**
 	 * Sets up theme defaults and registers support for various WordPress features.
 	 *
@@ -20,17 +21,18 @@ if ( ! function_exists( '_s_setup' ) ) :
 	 * runs before the init hook. The init hook is too late for some features, such
 	 * as indicating support for post thumbnails.
 	 */
-	function _s_setup() {
+	function _s_setup()
+	{
 		/*
 		 * Make theme available for translation.
 		 * Translations can be filed in the /languages/ directory.
 		 * If you're building a theme based on _s, use a find and replace
 		 * to change '_s' to the name of your theme in all the template files.
 		 */
-		load_theme_textdomain( '_s', get_template_directory() . '/languages' );
+		load_theme_textdomain('_s', get_template_directory() . '/languages');
 
 		// Add default posts and comments RSS feed links to head.
-		add_theme_support( 'automatic-feed-links' );
+		add_theme_support('automatic-feed-links');
 
 		/*
 		 * Let WordPress manage the document title.
@@ -38,19 +40,19 @@ if ( ! function_exists( '_s_setup' ) ) :
 		 * hard-coded <title> tag in the document head, and expect WordPress to
 		 * provide it for us.
 		 */
-		add_theme_support( 'title-tag' );
+		add_theme_support('title-tag');
 
 		/*
 		 * Enable support for Post Thumbnails on posts and pages.
 		 *
 		 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
 		 */
-		add_theme_support( 'post-thumbnails' );
+		add_theme_support('post-thumbnails');
 
 		// This theme uses wp_nav_menu() in one location.
 		register_nav_menus(
 			array(
-				'menu-1' => esc_html__( 'Primary', '_s' ),
+				'menu-1' => esc_html__('Primary', '_s'),
 			)
 		);
 
@@ -84,7 +86,7 @@ if ( ! function_exists( '_s_setup' ) ) :
 		);
 
 		// Add theme support for selective refresh for widgets.
-		add_theme_support( 'customize-selective-refresh-widgets' );
+		add_theme_support('customize-selective-refresh-widgets');
 
 		/**
 		 * Add support for core custom logo.
@@ -102,7 +104,7 @@ if ( ! function_exists( '_s_setup' ) ) :
 		);
 	}
 endif;
-add_action( 'after_setup_theme', '_s_setup' );
+add_action('after_setup_theme', '_s_setup');
 
 /**
  * Set the content width in pixels, based on the theme's design and stylesheet.
@@ -111,22 +113,24 @@ add_action( 'after_setup_theme', '_s_setup' );
  *
  * @global int $content_width
  */
-function _s_content_width() {
-	$GLOBALS['content_width'] = apply_filters( '_s_content_width', 640 );
+function _s_content_width()
+{
+	$GLOBALS['content_width'] = apply_filters('_s_content_width', 640);
 }
-add_action( 'after_setup_theme', '_s_content_width', 0 );
+add_action('after_setup_theme', '_s_content_width', 0);
 
 /**
  * Register widget area.
  *
  * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
  */
-function _s_widgets_init() {
+function _s_widgets_init()
+{
 	register_sidebar(
 		array(
-			'name'          => esc_html__( 'Sidebar', '_s' ),
+			'name'          => esc_html__('Sidebar', '_s'),
 			'id'            => 'sidebar-1',
-			'description'   => esc_html__( 'Add widgets here.', '_s' ),
+			'description'   => esc_html__('Add widgets here.', '_s'),
 			'before_widget' => '<section id="%1$s" class="widget %2$s">',
 			'after_widget'  => '</section>',
 			'before_title'  => '<h2 class="widget-title">',
@@ -134,23 +138,51 @@ function _s_widgets_init() {
 		)
 	);
 }
-add_action( 'widgets_init', '_s_widgets_init' );
+add_action('widgets_init', '_s_widgets_init');
+
+//デフォルトjQueryを読み込まない
+function delete_jquery() {
+	if (!is_admin()) {
+	  wp_deregister_script('jquery');
+	}
+  }
+  add_action('init', 'delete_jquery');
 
 /**
  * Enqueue scripts and styles.
  */
-function _s_scripts() {
-	wp_enqueue_style( 'style', get_template_directory_uri() . '/css/style.min.css' );
-	wp_style_add_data( '_s-style', 'rtl', 'replace' );
+function _s_scripts()
+{
+	//stylesheet読み込み
+	wp_enqueue_style('style', get_template_directory_uri() . '/css/style.min.css');
+	//fontawesome
+	wp_enqueue_style('fontawesome', 'https://use.fontawesome.com/releases/v5.6.3/css/all.css',array(), '5.6.3');
+	wp_style_add_data('_s-style', 'rtl', 'replace');
+	
+	
+	// jQuery
+	wp_enqueue_script('script_jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js', array(),'3.2.1', true);
+	//JS iScroll
+	wp_enqueue_script('script_JS_iScroll', 'https://cdnjs.cloudflare.com/ajax/libs/iScroll/5.2.0/iscroll.min.js', array(),'5.2.0', true);
+	// JS nav
+	wp_enqueue_script('JS_nav', get_template_directory_uri() . '/assets/js/nav.js', array(), _S_VERSION, true);
+	//JS yurayura
+	wp_enqueue_script('JS_yurayura', get_template_directory_uri() . '/assets/js/yurayura.js', array(), _S_VERSION, true);
+	//midnight
+	wp_enqueue_script('midnight', get_template_directory_uri() . '/assets/js/midnight.jquery.min.js', array(), _S_VERSION, true);
+	//colorbox
+	wp_enqueue_script('colorbox', get_template_directory_uri() . '/assets/js/jquery.colorbox-min.js', array(), _S_VERSION, true);
+	//main.js
+	wp_enqueue_script('main.js', get_template_directory_uri() . '/assets/js/main.js', array(), _S_VERSION, true);
 
-	// wp_enqueue_script( '_s-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
-
-	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-		wp_enqueue_script( 'comment-reply' );
+	if (is_singular() && comments_open() && get_option('thread_comments')) {
+		wp_enqueue_script('comment-reply');
 	}
 }
-add_action( 'wp_enqueue_scripts', '_s_scripts' );
+add_action('wp_enqueue_scripts', '_s_scripts');
 
+
+	
 /**
  * Implement the Custom Header feature.
  */
@@ -174,45 +206,47 @@ require get_template_directory() . '/inc/customizer.php';
 /**
  * Load Jetpack compatibility file.
  */
-if ( defined( 'JETPACK__VERSION' ) ) {
+if (defined('JETPACK__VERSION')) {
 	require get_template_directory() . '/inc/jetpack.php';
 }
 
 
-function Change_menulabel() {
-global $menu;
-global $submenu;
-$name = 'Design works All';
-$menu[5][0] = $name;
-$submenu['edit.php'][5][0] = $name.'一覧';
-$submenu['edit.php'][10][0] = '新しいDesign works';
+function Change_menulabel()
+{
+	global $menu;
+	global $submenu;
+	$name = 'Design works All';
+	$menu[5][0] = $name;
+	$submenu['edit.php'][5][0] = $name . '一覧';
+	$submenu['edit.php'][10][0] = '新しいDesign works';
 }
-function Change_objectlabel() {
-global $wp_post_types;
-$name = 'Design works All';
-$labels = &$wp_post_types['post']->labels;
-$labels->name = $name;
-$labels->singular_name = $name;
-$labels->add_new = _x('追加', $name);
-$labels->add_new_item = $name.'の新規追加';
-$labels->edit_item = $name.'の編集';
-$labels->new_item = '新規'.$name;
-$labels->view_item = $name.'を表示';
-$labels->search_items = $name.'を検索';
-$labels->not_found = $name.'が見つかりませんでした';
-$labels->not_found_in_trash = 'ゴミ箱に'.$name.'は見つかりませんでした';
+function Change_objectlabel()
+{
+	global $wp_post_types;
+	$name = 'Design works All';
+	$labels = &$wp_post_types['post']->labels;
+	$labels->name = $name;
+	$labels->singular_name = $name;
+	$labels->add_new = _x('追加', $name);
+	$labels->add_new_item = $name . 'の新規追加';
+	$labels->edit_item = $name . 'の編集';
+	$labels->new_item = '新規' . $name;
+	$labels->view_item = $name . 'を表示';
+	$labels->search_items = $name . 'を検索';
+	$labels->not_found = $name . 'が見つかりませんでした';
+	$labels->not_found_in_trash = 'ゴミ箱に' . $name . 'は見つかりませんでした';
 }
-add_action( 'init', 'Change_objectlabel' );
-add_action( 'admin_menu', 'Change_menulabel' );
+add_action('init', 'Change_objectlabel');
+add_action('admin_menu', 'Change_menulabel');
 
 
 // 投稿のアーカイブページを作成する
 function post_has_archive($args, $post_type)
 {
-    if ('post' == $post_type) {
-        $args['rewrite'] = true; // リライトを有効にする
-        $args['has_archive'] = 'archive'; // 任意のスラッグ名
-    }
-    return $args;
+	if ('post' == $post_type) {
+		$args['rewrite'] = true; // リライトを有効にする
+		$args['has_archive'] = 'archive'; // 任意のスラッグ名
+	}
+	return $args;
 }
 add_filter('register_post_type_args', 'post_has_archive', 10, 2);

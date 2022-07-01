c0b262f32251e7645b263bfe2a6b43c247d1b14d
TakumiYamaguchi <info@ivorytowerlab.jp>
2022-07-01 15:08:58 +0900

    title修正OK

--- header.php
+++ header.php
@@ -8,7 +8,7 @@
 
 <head>
     <meta charset="UTF-8">
-    <title>惡匠｜惡の匠で「わるだくみ」</title>
+    <title><?php wp_title(''); ?></title>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta name="viewport"
         content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, viewport-fit=cover">
